c3e2c9fbabf51da9e70824ed8b65e12d824be39f
James <james___3248@hotmail.com>
2019-04-15 09:25:28 +0100

    finished homework

--- specs/karaoke_bar_spec.rb
+++ specs/karaoke_bar_spec.rb
@@ -10,11 +10,11 @@ class TestGuests < Minitest::Test
 
   def setup
     @karaoke_bar = Karaoke_bar.new("Musical Chairs", [@room1, @room2], [@song1, @song2])
-    @room1 = Room.new()
-    @room2 = Room.new()
+    @room1 = Room.new([],[], 4)
+    @room2 = Room.new([],[], 2)
     @guest1 = Guest.new("Mark")
     @guest2 = Guest.new("Felipe")
-    @song1 = Song.new("There's a reson why")
+    @song1 = Song.new("There's a reason why")
     @song2 = Song.new("Supersonic")
 
   end
@@ -22,6 +22,5 @@ class TestGuests < Minitest::Test
   def test_bar_has_name
     assert_equal("Musical Chairs", @karaoke_bar.name)
   end
-
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -7,18 +7,40 @@ require_relative('../songs')
 class TestGuests < Minitest::Test
 
   def setup
-    @room1 = Room.new([], [])
+    @room1 = Room.new([@guest2, @guest3], [], 2, 3)
     @guest1 = Guest.new("Mark")
     @song1 = Song.new("Mardy Bum")
+    @guest2 = Guest.new("Jeff")
+    @guest3 = Guest.new("Jack")
   end
 
   def test_how_many_people_in_room
-    assert_equal(0, @room1.guests.count)
+    assert_equal(2, @room1.guests.count)
   end
 
   def test_room_can_have_song
       @room1.room_gets_song(@song1)
-      assert_equal("Mardy Bum", @room1.songs)
+      assert_equal(1, @room1.songs.count)
+  end
+
+  def test_room_can_have_guest
+    @room1.room_gets_guest(@guest1)
+    assert_equal(3, @room1.guests.count)
+  end
+
+  def test_guest_can_check_out
+    @room1.guest_check_out()
+    assert_equal(0, @room1.guests.count)
+  end
+
+  def test_check_capacity
+    assert_equal(2, @room1.capacity)
+  end
+
+  def test_room_cant_have_more_than_capacity
+      @room1.room_cant_have_more_than_capacity(@guest1)
+      @room1.room_cant_have_more_than_capacity(@guest1)
+    assert_equal(3, @room1.capacity)
   end
 
 end
